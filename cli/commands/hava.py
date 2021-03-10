65465efe67751703db283bb33eca027b5c623b5b
Simon Coope <simon@datarepublic.com>
2021-03-10 12:32:10 +1100

    Added remaining hava commands

--- cli/commands/hava.py
+++ cli/commands/hava.py
@@ -24,6 +24,14 @@ def cmd_list_sources(environments):
     results_for_output = utils.serialise_to_json(sources)
     print(results_for_output)
 
+@hava.command("get-source")
+@click.option("-i", "--id", help="The id of the source to retrieve")
+def cmd_get_source(id):
+    print('Getting source from Hava.')
+    source = hava_proxy.get_source(id)
+    results_for_output = utils.serialise_to_json(source)
+    print(results_for_output)
+
 @hava.command("get-env")
 @click.option("-i", "--id")
 def cmd_get_env(id):
@@ -32,3 +40,28 @@ def cmd_get_env(id):
 
     results_for_output = utils.serialise_to_json(env)
     print(results_for_output)
+
+@hava.command("get-stats")
+@click.option("-s", "--source", help="The source to get stats for.")
+def hv_get_stats(source):
+    
+    stats = dict(
+        sources=0,
+        envs=0
+    )
+
+    envs = []
+    if(source):
+        print('getting stats for source "{source}" from hava...'.format(source = source))
+        stats["sources"] = 1
+        envs = hava_proxy.get_env_count_by_source(source)
+    else:
+        print("getting stats of all hava resources...")
+        for source in hava_proxy.get_sources():
+            stats["sources"] += 1
+        envs = hava_proxy.get_envs()
+
+    for env in envs:
+        stats["envs"] += 1
+
+    print(utils.serialise_to_json(stats))
--- cli/proxies/hava_proxy.py
+++ cli/proxies/hava_proxy.py
@@ -2,41 +2,78 @@ from typing import List
 import requests
 import config
 from tqdm import tqdm
-from cli.utils import utils, simple_cache
-from cli.entities.hava_source import HavaSources
+from cli.utils import utils
+from cli.entities.hava_source import HavaSource, HavaSources
 from cli.entities.hava_environment import HavaEnvironment
 
-envs_cache = []
-env_cache = dict()
+envs_summary_cache = []
+envs_full_cache = dict()
+sources_full_cache = []
+
+# Don't make single source API call as it returns less information than getting all sources.
+def get_source(id):
+    if(id == None): raise ValueError('get_source expects "id" which was not supplied')
+    sources = get_sources()
+    for source in sources:
+        if(source.id == id):
+            return source
 
 def get_sources():
-    resp = requests.get(config.hava["api"].format(resource = "sources"), auth=(config.hava["username"], config.hava["password"]))
-    handled_response = utils.handle_response(resp)
-    return HavaSources(handled_response)
+    if(len(sources_full_cache) == 0):
+        resp = requests.get(config.hava["api"].format(resource = "sources"), auth=(config.hava["username"], config.hava["password"]))
+        handled_response = utils.handle_response(resp)
+        utils.append_to_array(sources_full_cache, HavaSources(handled_response))
+
+    return sources_full_cache
 
 # Due to the structure of the Hava API this implementation isn't optimal.  We have to get all environments, filter on the required source and then get each environment again
 # as the direct call to GET/env returns more data.
 def get_envs_by_source(source_id):
+    if(source_id == None): raise ValueError('get_envs_by_source expects "source_id" which was not supplied')
     result = []
 
     # Hit API for results and cache the results for subsequent calls in the same session.
-    if len(envs_cache) == 0:
-        resp = requests.get(config.hava["api"].format(resource = "environments?limit=1000"), auth=(config.hava["username"], config.hava["password"]))
-        handled_response = utils.handle_response(resp)["results"]
-        utils.append_to_array(envs_cache, handled_response)
-
-    for env in tqdm(envs_cache):
+    envs = get_envs()
+    for env in tqdm(envs):
         for sc in env["sources"]:
             if(sc["id"] == source_id):
                 result.append(get_env(env["id"]))
 
     return result
 
+def get_env_count_by_source(source_id):
+    if(source_id == None): raise ValueError('get_envs_by_source expects "source_id" which was not supplied')
+    result = []
+
+    envs = get_envs()
+    for env in envs:
+        for sc in env["sources"]:
+            if(sc["id"] == source_id):
+                result.append(env)
+    
+    return result
+
+def get_envs():
+    if len(envs_summary_cache) == 0:
+        resp = requests.get(config.hava["api"].format(resource = "environments?limit=1000"), auth=(config.hava["username"], config.hava["password"]))
+        handled_response = utils.handle_response(resp)["results"]
+        utils.append_to_array(envs_summary_cache, handled_response)
+
+    return envs_summary_cache
+
 def get_env(id):
-    if(id not in env_cache):
+    if(id == None): raise ValueError('get_env expects "id" which was not supplied')
+
+    if(id not in envs_full_cache):
         resp = requests.get(config.hava["api"].format(resource = "environments/" + id), auth=(config.hava["username"], config.hava["password"]))
         handled_response = utils.handle_response(resp)
         env =  HavaEnvironment(handled_response)
-        env_cache[env.id] = env
+        envs_full_cache[env.id] = env
     
-    return env_cache[env.id]
+    return envs_full_cache[env.id]
+
+def get_env_share_by_revision(environment_id, revision_id):
+    account_id = config.hava["account_id"]
+    url = config.hava["api"].format(resource = "environments/" + environment_id + "/share?account_id=" + account_id + "&revision_id=" + str(revision_id))
+    resp = requests.get(url, auth=(config.hava["username"], config.hava["password"]))
+    return utils.handle_response(resp)
